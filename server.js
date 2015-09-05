88796d5113e33411e72712d7b8e6163f46cd68a7
remy <remy.auricoste@gmail.com>
2015-09-05 23:29:40 +0200

    handle server conf

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var main = require("./src/main/js/main");
 
 var port = process.env.PORT || 8001;
+var serverFactory = process.env.PROTOCOL === "HTTP" ? main.XhrServer : main.SocketServer;
 
-new main.SocketServer(port);
+new serverFactory(port);
